2f4335102f1fc2460e064fc868d183fa15c813c7
Шумайлова Нина <ninashum@gmail.com>
2022-12-15 02:34:20 +0300

    commit ex12

--- homework_lesson1/ex12.py
+++ homework_lesson1/ex12.py
@@ -1,3 +1,15 @@
 # Напишите программу для. проверки 
 # истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
+
+X = []
+for i in range(3):
+        X.append(int(input("Введите значение: ")))
+print(X)
+left = not (X[0] or X[1] or X[2])
+right = not X[0] and not X[1] and not X[2]
+
+if left == right:
+    print("Утверждение истинно")
+else:
+    print("Утверждение ложно")
