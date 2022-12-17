2f1587658c8f6a4dd299191b993aeaa97cf68221
Шумайлова Нина <ninashum@gmail.com>
2022-12-18 00:39:25 +0300

    commit ex23

--- homework_lesson2/ex23.py
+++ homework_lesson2/ex23.py
@@ -2,3 +2,16 @@
 # НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, 
 # максимум использование библиотеки Random для и получения случайного int
 
+from random import randint
+list = []
+for i in range(5):
+    list.append(randint(0, 20))
+
+print ("Начальный список: " + str(list))
+
+from random import randint
+for i in range(len(list)-1, 0, -1):
+    j = randint(0, i + 1) 
+    list[i], list[j] = list[j], list[i] 
+print ("Перемешанный список: " +  str(list))
+
